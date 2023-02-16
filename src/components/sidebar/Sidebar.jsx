e17467f75145e7be463eadc738838e133748e610
Experadog <lsstudent008@mail.ru>
2023-02-16 18:54:04 +0600

    create sidebar and layout

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,11 +2,17 @@
 
 
 import React from 'react';
+import { Components } from '..';
+
+import cls from "../../assets/styles/sidebar/Sidebar.module.scss";
+import Logo from "../../assets/images/logo/insta.png";
 
 export default function Sidebar() {
   return (
-    <section className=''>
-      
+    <section className={cls.sidebar_wrapper}>
+      <div className={cls.sidebar_wrapper_content}>
+        <Components.Image src={Logo} />
+      </div>
     </section>
   )
 };
